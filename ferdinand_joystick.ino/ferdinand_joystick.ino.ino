b0e5218916ef9a64827b65f93fa17b59b1da0baa
David Sawatzke <david@sawatzk.de>
2017-01-27 18:59:01 +0100

    Initial Commit

---
+++ ferdinand_joystick.ino/ferdinand_joystick.ino.ino
@@ -0,0 +1,85 @@
+// Debug 
+#define debug
+
+#ifdef debug
+
+#else
+#include "DigiJoystick.h"
+#endif
+static const uint8_t sensors[] = {2, 0, 5};
+static const uint8_t pingOut = 1;
+
+static const uint32_t calibrationIn[sizeof(sensors)][2] = {
+  {200, 30000},
+  {200, 30000},
+  {200, 30000}
+};
+static const int8_t calibrationOut[sizeof(sensors)][2] = {
+  {-128, 127},
+  {0, 127},
+  {0, -128}
+};
+
+uint8_t data[8] = {128, 128, 128, 128, 128, 128, 0, 0};
+enum dataPos {
+  x = 0,
+  y = 1,
+  xrot = 2,
+  yrot = 3,
+  zrot = 4,
+  slider = 5,
+  buttonLowByte = 6,
+  buttonHighByte = 7
+};
+
+void setup() {
+  // Do nothing? It seems as if the USB hardware is ready to go on reset
+  pinMode(pingOut, OUTPUT);
+  for (uint8_t i = 0; i < sizeof(sensors); i++) {
+    pinMode(sensors[i], INPUT);
+  }
+}
+
+int8_t readSensor(uint8_t id) {
+  DigiJoystick.delay(10);
+  uint32_t rawData;
+  digitalWrite(pingOut, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(pingOut, LOW);
+  rawData = pulseIn(sensors[id], HIGH, 40000);
+  return map(rawData, calibrationIn[id][0], calibrationIn[id][1], calibrationOut[id][0], calibrationOut[id][1]);
+}
+
+
+void loop() {
+  DigiJoystick.setX((byte) readSensor(0));
+  DigiJoystick.setY((uint8_t)(readSensor(1) + readSensor(2)));
+  
+  // If not using plentiful DigiJoystick.delay() calls, make sure to
+  //DigiJoystick.update(); // call this at least every 50ms
+  // calling more often than that is fine
+  // this will actually only send the data every once in a while unless the data is different
+  
+  // you can set the values from a raw byte array with:
+  // char myBuf[8] = {
+  //   x, y, xrot, yrot, zrot, slider,
+  //   buttonLowByte, buttonHighByte
+  // };
+  // DigiJoystick.setValues(myBuf);
+  
+  // Or we can also set values like this:
+  //DigiJoystick.setX((byte) (millis() / 100)); // scroll X left to right repeatedly
+  //DigiJoystick.setY((byte) 0x30);
+  //DigiJoystick.setXROT((byte) 0x60);
+  //DigiJoystick.setYROT((byte) 0x90);
+  //DigiJoystick.setZROT((byte) 0xB0);
+  //DigiJoystick.setSLIDER((byte) 0xF0);
+  
+  // it's best to use DigiJoystick.delay() because it knows how to talk to
+  // the connected computer - otherwise the USB link can crash with the 
+  // regular arduino delay() function
+  DigiJoystick.delay(50); // wait 50 milliseconds
+  
+  // we can also set buttons like this (lowByte, highByte)
+  //DigiJoystick.setButtons(0x00, 0x00);
+}
